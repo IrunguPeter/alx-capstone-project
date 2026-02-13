474d8ec6781e7cb88a33fc4795edfa6ee529f9d8
Peter Irungu <Irungupeter204@gmail.com>
2026-02-14 00:15:42 +0300

    Added a virtual envronment for testing.. Python file too

--- capstone/Server/file.py
+++ capstone/Server/file.py
@@ -1,1 +1,10 @@
 #Python Code goes in here.. I will install the dependencies in a virtual evironment"
+import pypartpicker
+
+pcpp = pypartpicker.Client()
+part = pcpp.get_part("https://pcpartpicker.com/b/Fzhkcf")
+
+for spec, value in part.specs.items():
+    print(f"{spec}: {value}")
+
+print(part.cheapest_price)
