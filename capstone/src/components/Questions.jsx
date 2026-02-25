a3ff716367cf803fdf572bbb08674e011cf5e8ea
Peter Irungu <Irungupeter204@gmail.com>
2026-02-25 16:39:17 +0300

    Modified: Questions.jsx to have a form

--- capstone/src/components/Questions.jsx
+++ capstone/src/components/Questions.jsx
@@ -1,6 +1,26 @@
 import react from 'react'
 import { useState } from 'react'
 
+const Questions = () =>{
+    const [formData,setFormData]=useState({Use:'', Operatingsystem:'',portability:'',price:''});
 
+    const handleChange = (e)=>{
+        const {name,value}= e.target;
+        setFormData(prevState=>({...prevState,[name]:value}));
+    };
 
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        console.log(formData);
+    };
 
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="Use" value={formData.name} onchange={handleChange}/>
+            <button type="submit">Use</button>
+        </form>
+    );
+
+};
+
+export default Questions;
