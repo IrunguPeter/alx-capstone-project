b16f5a57fd7376efe63f6262b8efe9e6be28a9be
Peter Irungu <Irungupeter204@gmail.com>
2026-02-25 20:00:22 +0300

    Update: the header shows dad jokes

--- capstone/src/components/Header.jsx
+++ capstone/src/components/Header.jsx
@@ -4,18 +4,16 @@ import { useState, useEffect } from 'react';
 const Header = () => {
   const Url= `https://api.api-ninjas.com/v1/dadjokes`;
 
-  const [joke, setJoke] = useState('');
+  const [joke] = useState("");
 
   useEffect(() => {
-    const fetchData=async()=>{
+    const fetchData= async() => {
       const response = await fetch(Url,{
         headers: {
           "x-API-Key": process.env.X_API_KEY
         }
       });
       console.log(response);
-      const data = await response.json();
-      setJoke(data.joke);
     };
     fetchData();
   },[]);
@@ -31,6 +29,7 @@ const Header = () => {
           <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
+      <p>{joke}</p>
 
     </header>
     
